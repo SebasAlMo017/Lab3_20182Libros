e328ef4346eb607ca55818c8dd23d2821a0e0d9c
SebasAlMo017 <42181783+SebasAlMo017@users.noreply.github.com>
2018-09-28 14:13:42 -0500

    Update Libros.cpp

--- Libros.cpp
+++ Libros.cpp
@@ -1,74 +1,155 @@
 *//Sebastian Alvarado y Nelxon Villareal
 
-# Lab3_20182Libros
+*//Lab3_20182Libros
 
 #include <iostream>
+#include <string>
 
 using namespace std;
-class libro{
-	private:
+
+class libro 
+{
+private:
 	int codigo;
-	char *nombre;
-	char materia;
-	char *nombreautor;
-	bool estado;
-	
-	public:
-		libro(int cod,char *nom,char mat, char *nomaut,bool est);
-		int getcodigo();
-		char *getnombre();
-		char getmateria();
-		char *getnombreautor();
-		bool  getestado();
-		void setcodigo(int cod);
-		void setnombre(char *nom);
-		void setmateria(char mat);
-		void setnombreautor(char *nomau);
-		void setestado(bool est);
-	friend void buscar(char *nom,libro li[]);
+ 	string nombre;
+	char materialibro;
+	string nombreautor;
+	bool disponible;
+public:
+	libro *sig;
+	libro();
+	libro( int cod, string nom,char mat,string nomauto,bool dis);
+	int getcodigo();
+	string getnombre();
+	char getmaterialibro();
+	string getnombreautor();
+	bool getdisponibilidad();
+	void setcodigo(int cod);
+	void setnombre( string nom);
+	void setmaterialibro(char mat);
+	void setnombreautor(string nomauto);
+	void setdisponibilidad(bool dis);
+	friend void buscar(string nomauto ,libro &);
+	friend class libros;
 };
-void buscar(char *nom ,libro li []){
-	for(int j=0;j<5;j++){
-		if(nom== li[j].getnombre() or nom== li[j].getnombreautor() ){
-			cout<<"libro encontrado"<<endl;
+
+class libros
+{
+	private :
+	libro *li;
+	public:
+	libros(): li(NULL){}
+
+	bool disponible( libro l[],string nom){
+		for(int j=0;j<5;j++){
+		  if(l[j].getnombre() == nom){
+			return l[j].getdisponibilidad();
+		  }
 		}
+	return false;
 	}
+};
+
+void buscar(string nomauto, libro b[])
+{
+  bool res=false;
+
+  for(int j=0;j<5;j++){
+    if(b[j].getnombre() ==nomauto or b[j].getnombreautor()== nomauto )
+    {
+        res=true;
+    }
+  }
+  if(res)
+  {
+    cout<<"libro encontrado "<<endl;
+  }
+  else{
+    cout<<"libro no encontrado"<<endl;
+  }
 }
 
-libro::libro(int cod,char *nom, char mat, char *nomaut, bool est){
-	this->codigo = cod;
-	this->nombre =nom;
-	this->materia=mat;
-	this->nombreautor = nomaut;
-	this->estado= est;
+int main(int argc, char** argv)
+{
+  libro *li;
+  li = new libro[5];
+  li[0]= libro(1,"nom",'j',"jose",false);
+  li[1]= libro(1,"dosmil",'j',"nelson",false);
+  li[2]= libro(1,"mil",'j',"sebastian",true);
+  li[3]= libro(1,"docientos",'j',"jj",false);
+  li[4]= libro(1,"cien",'j',"pedro",false);
+  buscar("jose",li);
+  libros l;
+  bool result =l.disponible(li,"mil");
+  if(result)
+  {
+    cout<<"el libro esta disponible"<<endl;
+  }
+  else
+  {
+    cout<<"el libro no esta disponible"<<endl;
+  }
+
+  return 0;
 }
-int libro::getcodigo(){
-	return codigo;
+
+libro::libro(){}
+
+libro::libro(int cod,string nom,char mat,string nomauto,bool dis)
+{
+  codigo=cod;
+  nombre =nom;
+  materialibro=mat;
+  nombreautor= nomauto;
+  disponible= dis;
 }
-char * libro::getnombre(){
-	return nombre;
+
+int libro::getcodigo()
+{
+  return codigo;
 }
-char libro::getmateria(){
-	return materia;
+
+string libro::getnombre()
+{
+  return nombre;
 }
-char *libro::getnombreautor(){
-	return this->nombreautor;
+
+char libro::getmaterialibro()
+{
+ return materialibro;
 }
-bool libro::getestado(){
-	return estado;
+
+string libro::getnombreautor()
+{
+  return nombreautor;
 }
-void libro::setcodigo(int cod){
-	this->codigo = cod;
+
+bool libro::getdisponibilidad()
+{
+  return disponible;
 }
-void libro::setnombre(char *nom){
-	this->nombre = nom;
+
+void libro::setcodigo(int cod)
+{
+  codigo=cod;
 }
-void libro::setmateria(char mat){
-	this->materia=mat;
+
+void libro::setnombre(string nom)
+{
+  nombre=nom;
 }
-void libro::setnombreautor(char *nomaut){
-	this->nombreautor=nomaut;
+
+void libro::setmaterialibro(char mat)
+{
+  materialibro=mat;
 }
-int main(int argc, char** argv) {
-	return 0;
+
+void libro::setnombreautor(string nomauto)
+{
+  nombreautor=nomauto;
+}
+
+void libro::setdisponibilidad(bool dis)
+{
+  disponible= dis;
 }
